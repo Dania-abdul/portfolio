b33c099a7c50da59260f2d32522a96c14d585411
Dania Abdul <43537934+Dania-abdul@users.noreply.github.com>
2020-02-23 18:43:46 +0100

    Updates

---
+++ precache-manifest.f1e456f96ab25ff259c8f20fa469c4a3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1993d5b7de0c6742c27ae879dcc80d9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "680c625f2d559f9bcc49",
+    "url": "/portfolio/static/css/main.12904769.chunk.css"
+  },
+  {
+    "revision": "bfd8de4146d1fe70270d",
+    "url": "/portfolio/static/js/2.daaa3a4e.chunk.js"
+  },
+  {
+    "revision": "1af6562b6d659632968c2e5e85a5b301",
+    "url": "/portfolio/static/js/2.daaa3a4e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "680c625f2d559f9bcc49",
+    "url": "/portfolio/static/js/main.6c2782dc.chunk.js"
+  },
+  {
+    "revision": "f2b666a286fb3b518acf",
+    "url": "/portfolio/static/js/runtime-main.82ec84df.js"
+  },
+  {
+    "revision": "0d08163da4b8b79d14638ed8c51f4a58",
+    "url": "/portfolio/static/media/img-1.0d08163d.png"
+  },
+  {
+    "revision": "693d5c25a80e7a3349beb1dffe9a6526",
+    "url": "/portfolio/static/media/img-2.693d5c25.jpg"
+  }
+]);
