6cb33f803597d05ea23122fa56af1237d83f8ba8
dania_abdul <43537934+Dania-abdul@users.noreply.github.com>
2021-02-09 16:14:23 +0100

    Updates

---
+++ precache-manifest.f7e93d3ff167a062c10599b8ecbb7ddc.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fba1cf74ef1eb2b8ac4524e8c1332179",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "56816773412e1d86d6ba",
+    "url": "/portfolio/static/css/main.dce4361f.chunk.css"
+  },
+  {
+    "revision": "ab339ab194a8bddfc582",
+    "url": "/portfolio/static/js/2.31a5c062.chunk.js"
+  },
+  {
+    "revision": "1af6562b6d659632968c2e5e85a5b301",
+    "url": "/portfolio/static/js/2.31a5c062.chunk.js.LICENSE"
+  },
+  {
+    "revision": "56816773412e1d86d6ba",
+    "url": "/portfolio/static/js/main.75ca190a.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "e403fbd8fd021be0e58a1d1bc9b24b56",
+    "url": "/portfolio/static/media/BA-activity--light.e403fbd8.gif"
+  },
+  {
+    "revision": "36d083fff21e7d74817f2f556ee05318",
+    "url": "/portfolio/static/media/MCQ--LIGHT.36d083ff.gif"
+  },
+  {
+    "revision": "b132b0f5939d5616214b5f93f85c348c",
+    "url": "/portfolio/static/media/course-intro--light.b132b0f5.png"
+  },
+  {
+    "revision": "feb787de53ab7e7b98cb4579898bea74",
+    "url": "/portfolio/static/media/courses-list--switch.feb787de.gif"
+  },
+  {
+    "revision": "77a594b8138df2515e11552a4eb825d0",
+    "url": "/portfolio/static/media/dodici-single-product-gif.77a594b8.gif"
+  },
+  {
+    "revision": "594096997d474c7b5443b90bbd8c7537",
+    "url": "/portfolio/static/media/drag-drop--light.59409699.gif"
+  },
+  {
+    "revision": "4d07fb8fe574423314ac5d8411b5f710",
+    "url": "/portfolio/static/media/esse_img_1.4d07fb8f.jpg"
+  },
+  {
+    "revision": "3402ac8ed3ad4c2183c0fbfff7e7435a",
+    "url": "/portfolio/static/media/flip-card--light.3402ac8e.gif"
+  },
+  {
+    "revision": "e6dbb3bcde20c621ddb8f742fc5542d8",
+    "url": "/portfolio/static/media/front-projects--dodici.e6dbb3bc.jpg"
+  },
+  {
+    "revision": "fceeaa86d15b9b7ece10ad08fc6e95e4",
+    "url": "/portfolio/static/media/front-projects--esse.fceeaa86.jpg"
+  },
+  {
+    "revision": "933425ed9f351cfdacbdb605ba38f720",
+    "url": "/portfolio/static/media/front-projects--grandeomega.933425ed.png"
+  },
+  {
+    "revision": "693d5c25a80e7a3349beb1dffe9a6526",
+    "url": "/portfolio/static/media/img-2.693d5c25.jpg"
+  },
+  {
+    "revision": "2b9b8fbd6e857694fc95cf82491eaecf",
+    "url": "/portfolio/static/media/intro-photo.2b9b8fbd.jpg"
+  },
+  {
+    "revision": "f6b444b5c37da0fb262abe41085f213a",
+    "url": "/portfolio/static/media/photo-bw.f6b444b5.png"
+  },
+  {
+    "revision": "21914cec31834aebfdd9a613e2621645",
+    "url": "/portfolio/static/media/post-dodici-image-1.21914cec.jpg"
+  },
+  {
+    "revision": "b18986561b15985942260186313614df",
+    "url": "/portfolio/static/media/post-dodici-image-2.b1898656.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.1ef41886eb8373542031a8d02efe0e14.js"
+  "/portfolio/precache-manifest.f7e93d3ff167a062c10599b8ecbb7ddc.js"
 );
 
 self.addEventListener('message', (event) => {
